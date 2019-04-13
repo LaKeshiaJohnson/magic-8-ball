c8c48f55cb7a10a951f0763928552b9d617f16fe
lakeshia <ljohnson2k@gmail.com>
2019-04-13 13:42:36 -0500

    added image swap, shake effect, and reset btn

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ $(document).ready(function(){
 	var magic8Ball = {};
 	magic8Ball.listOfAnswers = ["yes", "no", "ask again later", "chances look good", "positively", "very doubtful", "outlook not so good", "better not tell you now"];
 	magic8Ball.askQuestion = function (question) {
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+		$("#answer").fadeIn(4000);
 		var randomNum = Math.random() * this.listOfAnswers.length;
 		var roundedNum = Math.floor(randomNum);
 		var answer = this.listOfAnswers[roundedNum];
 		console.log(question, answer);
 		$("#answer").text(answer);
 	};
+	$("#answer").hide();
+	
 	var onClick = function() {
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
  
-    var question = prompt("ASK A YES/NO QUESTION!");
-    magic8Ball.askQuestion(question);
-};
- 
-$("#questionButton").click( onClick );
+		//wait half a second before showing prompt
+	   setTimeout(
+	       function() {
+	           //show prompt
+	           var question = prompt("Ask a yes or no question");
+	           $("#8ball").effect( "shake" );
+	           magic8Ball.askQuestion(question);
+	       }, 500);
+	};
+
+	var reset = function() {
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+		$("#answer").hide();
+	};
+
+	$("#questionButton").click( onClick );
+	$("#resetButton").click( reset );
 });
